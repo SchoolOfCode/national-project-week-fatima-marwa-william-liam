06e703251a6304807cee3d5f735f5b5c0cc16486
marwaismail99 <ismailmarwa.563@gmail.com>
2022-01-17 17:13:46 +0000

    Frontend and backend separated

--- backend/config.js
+++
@@ -1,5 +0,0 @@
-const host = process.env.PGHOST;
-const user = process.env.PGUSER;
-const database = process.env.PGDATABASE;
-const password = process.env.PGPASSWORD;
-const port = process.env.PGPORT;
