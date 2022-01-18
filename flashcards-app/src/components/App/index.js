bc7f620e993e7d3809f2727ec422deb64757a60e
fatima-hashi <fmhashi91@gmail.com>
2022-01-18 14:39:48 +0000

    creater router paths for all pages

--- flashcards-app/src/components/App/index.js
+++ flashcards-app/src/components/App/index.js
@@ -1,6 +1,6 @@
 import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
-import { useState, useEffect } from "react";
 import Home from "../Home/index";
 import HTML from "../HTML/index";
 import CSS from "../CSS/index";
@@ -9,18 +9,14 @@ import JS from "../JS/index";
 //const question = { question: "What does HTML stand for?", answer: "HyperText Markup Language"};
 
 function App() {
-  const [isFlipped, setIsFlipped] = useState(false);
-
-  function toggleFlipped() {
-    setIsFlipped(!isFlipped);
-  }
-
   return (
     <div className="App">
-      <Home />
-      <HTML />
-      <CSS />
-      <JS />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/html" element={<HTML />} />
+        <Route path="/css" element={<CSS />} />
+        <Route path="/js" element={<JS />} />
+      </Routes>
     </div>
   );
 }
