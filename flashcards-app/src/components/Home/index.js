779719519f96887c8e0633008e65b1902f10882c
marwaismail99 <ismailmarwa.563@gmail.com>
2022-01-18 15:36:48 +0000

    Homepage created

--- flashcards-app/src/components/Home/index.js
+++ flashcards-app/src/components/Home/index.js
@@ -1,9 +1,22 @@
 import React from "react";
+import Header from "../Header/Header.js";
+import Footer from "../Footer/Footer.js";
 
 function Home() {
-  return <div>
-    <h1>Hello</h1>
-  </div>;
+  return (
+    <div>
+      <Header />
+      <h2>Revision made easy with</h2>
+      <h1>DevCards.</h1>
+      <h3>Choose a category to see relevant question/answer cards.</h3>
+      <div class="options">
+        <button class="HTML">HTML</button>
+        <button class="CSS">CSS</button>
+        <button class="JavaScript">JavaScript</button>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default Home;
