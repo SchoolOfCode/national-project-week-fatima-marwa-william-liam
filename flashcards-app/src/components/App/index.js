a03582ecc7c064714d12118b6cc2de87d026e826
fatima-hashi <fmhashi91@gmail.com>
2022-01-18 09:57:04 +0000

    setting up isFlipped toggle

--- flashcards-app/src/components/App/index.js
+++ flashcards-app/src/components/App/index.js
@@ -1,7 +1,25 @@
+import React from "react";
 import "./App.css";
+import {useState, useEffect} from "react";
+import Flashcard from "../Flashcard/index";
+import Home from "../Home/index" ;
+import HTML from "../HTML/index";
+
 
 function App() {
-  return <div className="App"></div>;
+  const [isFlipped, setIsFlipped] = useState(false);
+const [questions, setQuestions] = useState([]);
+
+function toggleFlipped(){
+  setIsFlipped(!isFlipped);
+}
+
+  return <div className="App">
+    <Home />
+    <HTML />
+    <CSS />
+    <JS />
+  </div>;
 }
 
 export default App;
